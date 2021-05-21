1b24949cd279d40e2c42d88c07fb125648a504ae
luuhwinck <maindsetluuh@gmail.com>
2021-05-21 12:05:03 -0300

    20 commit

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 Route::post("/resultado", [CalculatorController::class,'getResult'])->name('resultado');
