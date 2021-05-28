20def4ef2f1e1c6761003566eb99ec06a564eafa
luuhwinck <maindsetluuh@gmail.com>
2021-05-28 00:58:31 -0300

    atualização das views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,93 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Projeto Calculadora</title>
+
+
 </head>
 <body>
 
     <form action="{{route('resultado')}}" method="post">
         @csrf
         <table>
+			<legend>Projeto Calculadora</legend>
 <tr>
-	<td><input type="number" name="primeiro" /></td>
+	<td><input type="number" name="primeiro" placeholder="Digite um numero" /></td>
 	<td>
-	<select name="operacao">
+	<div class="operacao">
+		<input id="adi" type="radio" name="operacao" value="+" checked><label for="adi">+</label>
+		<input id="sub" type="radio" name="operacao" value="-"><label for="sub">-</label>
+		<input id="mul" type="radio" name="operacao" value="*"><label for="mul">*</label>
+		<input id="div" type="radio" name="operacao" value="/"><label for="div">/</label>
+	</div>
+	
+	<!--<select name="operacao">
 		<option value="+" >+</option>
 		<option value="-">-</option>
 		<option value="*">*</option>
 		<option value="/">/</option>
-	</select>
-	
+	</select>-->
+
+
 	</td>
-	<td><input type="number" name="segundo" /></td>
+	<td><input type="number" name="segundo" placeholder="Digite um numero" /></td>
 	<td> <input type="submit" value="enviar"> </td>
 </tr>
 </table>
     </form>
+
+	<style>
+		table{
+			margin: 10px auto 10px auto;
+			width: 30%;
+			border: solid black 1px;
+			padding: 3%
+		}
+
+		legend{
+			text-align: center;
+			padding: 6px;
+			margin: 10px;
+			border: solid black 0.5px;
+			font-size: 100%;
+			font-weight: bold;
+			background-color: #e8e8e8;
+		}
+		
+		td{
+			display: block;
+			font-size: 13px;
+		}
+		input{
+			width: 100%;
+			border: solid #ccc 1px;
+			font-size: 13px;
+			margin-bottom: 10px;
+		}
+		input[name=operacao]{
+			display: none;
+		}
+
+		label{
+			width: 21%; border: solid #ccc 0.5px; 
+		    text-align: center; line-height: 22px;
+		}
+
+		input[name=operacao]:checked +label{
+			color: white; font-size: 20px; font-weight: bold; background: #3336;
+		}
+
+		div {
+			display: flex;
+			justify-content: space-around;	margin-bottom: 10px;
+		}
+		input[value=enviar]:hover{
+			background-color:  #3333;
+		}
+
+	</style>
 </body>
 </html>
--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -1,5 +1,108 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        :root{
+  --corhead: #999;
+    }
+*{
+  margin: 0;
+    padding: 0;
+}
+body,#container_all{
+  position: absolute;
+  width: 100%; height: 100%;
+}
+    table{
+      border: 1px solid transparent;
+      border-collapse: collapse;
+    }
+    td{
+      border: 1px solid ;  text-align: center;  padding: 20px; font-size: 19px;
+    }
+    th{
+      border: 1px solid ;  padding: 20px;
+    }
+    
+    thead tr th:first-of-type{
+      background: var(--corhead);
+      border-radius:  15px 0 0 0;
+    }
+    
+    thead tr th{
+      background: var(--corhead);
+    }
+    
+    thead tr th:last-of-type{
+      background: var(--corhead);
+      border-radius:  0 15px 0 0;
+    }
+    
+    tfoot tr td{
+      background: var(--corhead);;
+      border-radius: 0 0 15px 15px;
+      border: 1px solid ;
+    }
 
-echo $primeiro." ".$operacao." ".$segundo."=".$total;
-?>
-<button><a href="{{route('home')}}">Clique aqui para voltar!</a></button>
+div#table{
+  border-radius: 17px;
+  border: 2px inset;
+  box-sizing: border-box; overflow: hidden;
+  box-shadow: 4px 4px 4px black;
+}
+tfoot td{
+  width: 100%; padding: 0; overflow: hidden;
+}
+td button{
+  height: 64px; width: 296.2px; background: transparent;
+}
+#container_all{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
+
+</head>
+<body>
+
+
+    <div id="container_all">
+        <div id="table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>1º valor</th>
+                        <th>operação</th>
+                        <th>2º valor: </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{$primeiro}}</td>
+                        <td>{{$operacao}}</td>
+                        <td>{{$segundo}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">{{$total}}</td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan='3'><button><a href="{{route('home')}}">Clique aqui para voltar!</a></button></td>
+                    </tr>
+                </tfoot>
+            </table>
+
+        </div>
+    </div>
+
+    
+</body>
+</html>
+
